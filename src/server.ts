8cc7812ac7b449c20a65099397c3260347c1407d
Matheus França <matheus.franca@opovo.com.br>
2024-10-31 08:59:26 -0300

    feat: create all colleges router

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import getColleges from '@/routes/college/get-colleges';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
@@ -7,6 +8,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(getColleges);
 
 app.listen(process.env.PORT, () => {
   return `HTTPS Server is running on port: ${process.env.PORT}`;
